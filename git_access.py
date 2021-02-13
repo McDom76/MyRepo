465772da4b925630484648b50d1b663facc2da98
Dominik <lemis@web.de>
2021-02-13 12:57:54 +0100

    find login data in cmdout

--- git_access.py
+++ git_access.py
@@ -1,11 +1,13 @@
 import json
 import subprocess
 
-output = subprocess.check_output("curl https://api.github.com/users/McDom76", shell=True)
+outxbytes = subprocess.check_output("curl https://api.github.com/users/McDom76", shell=True)
 
-
-
+outcmd = str(outxbytes)
+text = ["login: hennd"]
+list_output = outcmd.split(",\\n")
+pos_login = outcmd.find("login")
 
 print("Now cmdout:")
-print(output)
+print(pos_login)
 print("\n ----------------")
